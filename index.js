6bc32a5207774aaf364d4014ccfdde41110ec598
Temuulen Batbileg <23LP8360@G401-CC0107.local>
2024-02-19 15:51:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// // module
+// import bla from "bla bla";
+// // commonJs
+// const bla = require(`bla bla`);
+
+const express = require("express");
+const { products, users } = require("./dummy.json");
+
+// console.log(products);
+// console.log(users);
+
+const app = express();
+
+app.get("/products", (req, res) => {
+  res.type = "application/json";
+  res.send({ products });
+});
+app.get("/users", (req, res) => {
+  res.type = "application/json";
+  res.send({ users });
+});
+
+app.get("/usernames", (req, res) => {
+  res.type = "application/json";
+  res.send.filter({ usernames });
+});
+
+const usernames = users.map((e) => {
+  return e.name;
+});
+// console.log(usernames);
+
+app.listen(3001, () => {
+  console.log("Server is listening at port 3001");
+});

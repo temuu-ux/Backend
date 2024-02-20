b9f56471f0275a5ca04e42610328fcbbb55856cf
Temuulen Batbileg <23LP8360@G401-CC0107.local>
2024-02-20 14:05:15 +0800

    jfivn

--- index.js
+++ index.js
@@ -3,32 +3,78 @@
 // // commonJs
 // const bla = require(`bla bla`);
 
+// console.log(products);
+// // console.log(users);
+// const express = require("express");
+// const { products, users } = require("./dummy.json");
+// const app = express();
+
+// app.get("/products", (req, res) => {
+//   res.type = "application/json";
+//   res.send({ products: products });
+// });
+// app.get("/users", (req, res) => {
+//   res.type = "application/json";
+//   res.send({ users });
+// });
+
+// app.get("/usernames", (req, res) => {
+//   res.type = "application/json";
+//   res.send({ usernames });
+// });
+
+// const usernames = users.map((e) => {
+//   return e.name;
+// });
+// console.log(usernames);
+
+// app.listen(3001, () => {
+//   console.log("Server is listening ");
+// });
 const express = require("express");
 const { products, users } = require("./dummy.json");
-
-// console.log(products);
-// console.log(users);
-
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const fs = require("fs");
 
-app.get("/products", (req, res) => {
-  // res.type = "application/json";
-  res.send({ products });
-});
-app.get("/users", (req, res) => {
-  res.type = "application/json";
-  res.send({ users });
-});
-
-app.get("/usernames", (req, res) => {
-  res.type = "application/json";
-  res.send({ usernames });
+app.get("/products", (request, response) => {
+  response.type = "application/json";
+  response.status(200);
+  response.send({ products });
 });
 
-const usernames = users.map((e) => {
-  return e.name;
+app.get("/users", (request, response) => {
+  response.type = "application/json";
+  response.send({ abc: users });
 });
-// console.log(usernames);
+
+app.post("/add-user", (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+
+  fs.readFile("dummy.json", (error, data) => {
+    if (error) {
+      console.log("Error in reading file");
+    } else {
+      const jsonFile = JSON.parse(data.toString());
+      jsonFile.users.push(newUser);
+      fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+        if (err) {
+          console.log(err);
+          res.send("error happened");
+        } else {
+          console.log("success");
+          res.send("User added successfully");
+        }
+      });
+    }
+  });
+  res.status(200);
+  res.send("User added successfully");
+});
+
+app.post("/read-new-user", (req, res) => {});
 
 app.listen(3001, () => {
   console.log("Server is listening at port 3001");
